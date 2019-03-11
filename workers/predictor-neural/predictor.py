df78d236ad33d6ad6533a79f5a42fd9169b30d85
nalenz <julian.nalenz@tum.de>
2019-03-11 18:12:14 +0100

    Implement Python predictor framework

---
+++ workers/predictor-neural/predictor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+#import keras
+#import numpy as np
+#from keras.models import Sequential, load_model
+#from keras.layers import Activation, Dropout, Flatten, Dense
+
+from utils import *
+
+
+
+# read character training data
+charsTrain = readMLDataFile("chars-to-train")
+rangedAttrs = ["books", "dateOfBirth", "dateOfDeath", "house", "locations", "titles"]
+attrRanges = extremeAttrArray(charsTrain, rangedAttrs);
+print(attrRanges)
+sys.exit(0)
+
+
+
+# build the model
+model = Sequential()
+model.add(Dense(250, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+# train the model
+model.fit(data, labels, epochs=50, batch_size=16)
+model.save('got-predictor-model.h5')
---
+++ workers/predictor-neural/utils.py
@@ -0,0 +1,30 @@
+import os
+import json
+import math
+
+
+
+# some global constants
+dirname = os.path.dirname(__file__)
+
+
+
+# find maximum attribute value in list of dicts
+def accAttr(arr, attr, fn, alt):
+  return fn(map(lambda x: x[attr] if not isinstance(x[attr], list) else (alt if len(x[attr])==0 else fn(x[attr])), arr))
+
+def minAttr(arr, attr):
+  return accAttr(arr, attr, min, math.inf)
+
+def maxAttr(arr, attr):
+  return accAttr(arr, attr, max, -math.inf)
+
+def extremeAttr(arr, attr):
+  return [minAttr(arr, attr), maxAttr(arr, attr)]
+
+def extremeAttrArray(arr, attrs):
+  return dict(zip(attrs, map(lambda x: extremeAttr(arr, x), attrs)))
+
+def readMLDataFile(name):
+  with open(os.path.join(dirname, "../formatter/output/ml-data/" + name + ".json"), "r") as f:
+    return json.load(f)
