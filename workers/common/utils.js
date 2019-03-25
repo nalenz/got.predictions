38aad02d53ff3b3c071737dfa2e4346406e8afe2
nalenz <julian.nalenz@tum.de>
2019-03-25 15:43:55 +0100

    Add some JS utility functions

--- workers/common/utils.js
+++ workers/common/utils.js
@@ -31,13 +31,23 @@ function loadFormatterMLData(name) {
 }
 
 /**
+ * Loads a JSON file which contains preprocessed information from the GoT TV show used by the
+ * machine learning scripts.
+ * @param {string} name - The type of ML data, namely `chars-to-train` for the characters
+ * training shall happen with or `chars-to-predict` for the characters the predictions will be made for.
+ */
+function loadFormatterShowMLData(name) {
+  return fs.readJSON(path.join(__dirname, `../formatter-show/output/ml-data/${name}.json`));
+}
+
+/**
  * Writes some generic data to the `output` directory, relative to the main script's path.
  * Returns the written data unmodified.
  * @param {string} name - The name of the output file, without extension.
  * @param {(object|array)} data - The data whose serialized version is written to the respective file.
  */
-async function writeOutputData(name, data) {
-  await fs.writeJSON(path.join(dirnameMain, `output/${name}.json`), data);
+async function writeOutputData(name, data, pretty) {
+  await fs.writeJSON(path.join(dirnameMain, `output/${name}.json`), data, pretty ? { spaces: 4 } : undefined);
   return data;
 }
 
@@ -86,9 +96,9 @@ async function writeOutputDataBinary(name, data, compress) {
 /**
  * Uniquifies and sorts all entries of the given array which have been modified by the given
  * function before. For an example, see the `createSetFromAttr` function, which is a specialization
- * of this one. Note that this function is only designed to deal with string arrays. It
- * automatically filters out invalid values (e.g. null or empty string) and converts every string
- * to its lower case version before doing any further processing.
+ * of this one. Note that this function is only designed to deal with string arrays. It automatically
+ * filters out invalid values (e.g. null or empty string), converts every string to its lower case
+ * version and removes leading or trailing quotes before doing any further processing.
  * @param {object[]} arr - The array to be processed.
  * @param {function} fn - The function to be applied to every element of this array. It always needs
  * to return an array of values itself.
@@ -96,7 +106,12 @@ async function writeOutputDataBinary(name, data, compress) {
 function createSetFromAttrFunc(arr, fn) {
   return [...new Set([].concat(...arr.map(c => fn(c).map(x => (x || '').toLowerCase()))))]
     .filter(c => c.length > 0)
-    .map(c => c.replace(/&apos;/g, "'"))
+    .map(c =>
+      c
+        .replace(/(&apos;|\u2019)/g, "'")
+        .replace(/^"/g, '')
+        .replace(/"$/g, ''),
+    )
     .sort();
 }
 
@@ -117,11 +132,24 @@ function createSetFromAttr(arr, attr) {
 /**
  * Converts an array of actual values to indices in a base array, while eliminating invalid ones,
  * e.g. `arrToIndices(["a","b","a","c","c","d"], ["a","b","c"])  ==  [1,2,1,3,3]`.
+ * Is supposed to used in correspondance with `createSetFromAttr`, i.e. also acts on the lower
+ * case version of all string as well.
  * @param {string[]} arr - The array with values, most of which should appear in the `base` array.
  * @param {string[]} base - The list of base values to be used.
  */
 function arrToIndices(arr, base) {
   return (arr || []).map(x => base.indexOf(x.toLowerCase())).filter(x => x !== -1);
+}
+
+/**
+ * Determines how many values are given in the supplied object in total for the given list of
+ * attributes. If an attribute is not set, it counts 0, if it's a single value it counts 1,
+ * and if it's an array, it counts how many values are in this array.
+ * @param {*} obj - The object to be looked at.
+ * @param {*} attrs - The list of attributes the object will be searched through for.
+ */
+function countAttrValues(obj, attrs) {
+  return attrs.map(a => (obj[a] instanceof Array ? obj[a].length : obj[a] ? 1 : 0)).reduce((a, b) => a + b);
 }
 
 function accumulateAttr(arr, attr, fn, alt) {
@@ -151,6 +179,12 @@ function maxAttr(arr, attr) {
   return accumulateAttr(arr, attr, a => Math.max(...a), -Infinity);
 }
 
+/**
+ * Finds the range of values of an attribute in a list of objects. The result is an object
+ * with the `min`, `max` and `span` (i.e. `max - min + 1`) attributes.
+ * @param {object[]} arr - The array to be processed.
+ * @param {string} attr - The attribute that will be used to determine the range of.
+ */
 function extremeAttr(arr, attr) {
   let ret = { min: minAttr(arr, attr), max: maxAttr(arr, attr) };
   ret.span = ret.max - ret.min + 1;
@@ -245,12 +279,15 @@ module.exports = {
   loadBookData,
   loadShowData,
   loadFormatterMLData,
+  loadFormatterShowMLData,
   writeOutputData,
   writeOutputDataBinary,
   createSetFromAttrFunc,
   arrToIndices,
+  countAttrValues,
   minAttr,
   maxAttr,
+  extremeAttr,
   clamp,
   writeJSONSetFromAttr,
   JoinedOneHotVector,
