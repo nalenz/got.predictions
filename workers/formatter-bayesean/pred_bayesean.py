7865d588394089fb0d38a12f756aa917b5985fc8
valentindimov <balentin.dimov@gmail.com>
2019-03-26 15:26:15 +0100

    Updated the Python predictor script to write its output to a JSON file (predictorOutput.json). Removed some unnecessary code/comments.

--- workers/formatter-bayesean/pred_bayesean.py
+++ workers/formatter-bayesean/pred_bayesean.py
@@ -6,6 +6,7 @@ import pymc3 as pm
 from pymc3.distributions.timeseries import GaussianRandomWalk
 from theano import tensor as T
 import random
+import json
 
 #read input file
 df = pd.read_json(path_or_buf = "ref_chs.json", typ = "frame")
@@ -34,12 +35,10 @@ exposure=exposure.astype(np.float) #keep it as a float for calculation purposes
 filter_func = np.vectorize(lambda v: 1e-200 if v<=0 else v) #assuming a tiny chance of dying after you're dead isn't so bad, is it?
 exposure = filter_func(exposure)
 
-#We can filter out only the columns we'll use for learning (as a test)
-
-#df = pd.concat([df.male, df.isHeir, df.numTitles, df.numSpouses], axis=1) #TODO take only a few columns as a test
-
-#convert the DataFrame into a numPy array (also exclude columns we don't want to have)
-df_num=df.drop(["age", "isDead", "name"], axis=1).to_numpy().astype(float) #characters=rows, attributes=cols
+#convert the DataFrame into a numPy array (also exclude columns we don't want to have as training parameters)
+df_dropped = df.drop(["age", "isDead", "name"], axis=1)
+colNames = df_dropped.columns.values.tolist() #will use later when writing the prediction file
+df_num=df_dropped.to_numpy().astype(float) #characters=rows, attributes=cols
 num_parameters = df_num.shape[1];
 
 SEED = random.randint(1,10000000) #will be used in the sampler
@@ -51,17 +50,14 @@ with pm.Model() as model:
   mu = pm.Deterministic('mu', exposure*lambda_) #this is also a matrix (risk = 0 if character already dead, otherwise same as lambda_)
   obs = pm.Poisson('obs', mu, observed=death) 
   
-n_samples = 100 #both should be 1000, 100 for quick testing
-n_tune = 100
+n_samples = 1000 #both should be 1000, 100 for quick testing
+n_tune = 1000
 #now, sample the model
 with model:
-  trace = pm.sample(n_samples, tune = n_tune, random_seed=SEED) #nuts_kwargs = {"target_accept":0.95}
+  trace = pm.sample(n_samples, tune = n_tune, random_seed=SEED)
   
 # trace['beta'] is a matrix. Rows = all the samples, colums = sampled beta vector
 # trace['lambda'] is a matrix, rows = all the samples, cols = sampled chance to die in a given time slice
-
-mean_beta = trace['beta'].mean(axis=0) # make a mean of all rows in the entire trace, transform the column matrix into a (single-) row matrix and get the row out
-print(np.exp(mean_beta))
 
 def get_dotprodfactors(params): #get the hazard multipliers (not yet exponentiated) for each step of the trace, depending on the parameters
   return trace['beta'].dot(np.atleast_2d(params).transpose())
@@ -78,3 +74,34 @@ def survival(hazard): #describes likelihood of surviving the cumulative hazard
 def survivalParams(params): #describes survival function, given some params
   return survival(calc_hazard(get_dotprodfactors(params)))
   
+def meanSurvivalParams(params): #takes the mean out of the whole trace for the survival function
+  return survivalParams(params).mean(axis=0)
+  
+def fitAge(survivalFn, chance): #given a survial function (vector of probabilities), find the highest age with over 'chance' chance of survival
+  return (n_intervals-np.searchsorted(np.flip(survivalFn), chance))*interval_length
+  
+#Now construct the output file
+predictions = {} #we'll write this dict to a JSON
+predictions["priorHazard"] = trace['lambda0'].mean(axis=0).astype(float).tolist()
+predictions["attributes"] = colNames
+mean_beta = trace['beta'].mean(axis=0) # make a mean of all rows in the entire trace, transform the column matrix into a (single-) row matrix and get the row out
+predictions["meanBetaExp"] = np.exp(mean_beta).astype(float).tolist()
+predictions["characters"] = []
+#now add the survial function for every character
+for i in range(0, num_characters):
+  ch = {} #this dict will represent the character's survival function
+  ch["name"] = df["name"][i]
+  ch["alive"] = False if df["isDead"][i] > 0 else True
+  ch["age"] = df["age"].astype(float)[i]
+  survFn= meanSurvivalParams(df_num[i, :]).astype(float).tolist() #take the i-th row of df_num for the character's parameters
+  ch["predictedSurvivalAge"] = fitAge(survFn, 0.5).astype(float)
+  confidence = 0.8
+  ch["confIntervalLower"] = fitAge(survFn, confidence).astype(float)
+  ch["confIntervalHigher"] = fitAge(survFn, 1-confidence).astype(float)
+  ch["confIntervalConfidence"] = confidence
+  ch["survivalFunction"] = survFn
+  predictions["characters"].append(ch)
+  
+#now write the predictions object to a file
+output = open('predictorOutput.json', 'w')
+json.dump(predictions, output, indent=2)
