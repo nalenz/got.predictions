dd1f2c92ca031fe82dec322d40b40d349ae6c0f1
Valentin Dimov <balentin.dimov@gmail.com>
2019-03-28 18:45:52 +0100

    fml

--- workers/common/config.js
+++ workers/common/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   GOT_CURRENT_YEAR: 304,
   AGE_MAXIMUM: 100,
-  GOT_SHOW_BEGIN = 298; //first year the show took place
+  GOT_SHOW_BEGIN = 298 //first year the show took place
 };
