afaf8a72eee3afabcfc450d903dbbb1efc759381
nalenz <julian.nalenz@tum.de>
2019-04-03 22:10:27 +0200

    Edit simple prediction updater to update everything

--- workers/uploader-predictions/index.js
+++ workers/uploader-predictions/index.js
@@ -1,19 +1,21 @@
 const utils = require('../common/utils');
 const config = require('../common/config');
-const request = require('request-promise-native');
-const querystring = require('querystring');
 
 (async () => {
   const [bookPred, showPred] = await Promise.all([utils.loadPredictionsBook(), utils.loadPredictionsShow()]);
-  const formData = {
-    slug: 'Jon_Snow',
-    longevity: [0.3, 0.4],
-    longevityStart: 304,
-    plod: 42,
-    token: '123secure',
-  };
-  const apiUrl = 'https://gotdata.northeurope.cloudapp.azure.com/api/book/characters/updateGroupB';
+  const updater = await new utils.APIUpdater().init();
 
-  let res = await request.post(apiUrl, { json: formData });
-  console.log(res);
+  // update book predictions on server
+  for (let name in bookPred) {
+    console.log(await updater.updatePLODLongevity('book', name, bookPred[name], config.GOT_CURRENT_YEAR_BOOK, 1 - bookPred[name][0]));
+  }
+
+  // update show predictions on server
+  for (let name in showPred) {
+    console.log(await updater.updatePLODLongevity('show', name, showPred[name], config.GOT_CURRENT_YEAR_SHOW, 1 - showPred[name][0]));
+  }
+
+  // output some final statistics
+  console.log(`successfully updated ${Object.keys(bookPred).length} book predictions`);
+  console.log(`successfully updated ${Object.keys(showPred).length} show predictions`);
 })();
