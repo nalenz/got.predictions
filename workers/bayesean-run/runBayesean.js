98228cfc876e7a00192216ca34f5ab3ce22e2c88
Valentin Dimov <balentin.dimov@gmail.com>
2019-04-03 18:46:09 +0200

    Removed unnecessary files.

--- workers/bayesean-run/runBayesean.js
+++
@@ -1,9 +0,0 @@
-'use scrict';
-
-const {formatBookData} = require('../formatter-bayesean-book/index.js'); //(outfile, callback)
-const {formatShowData} = require('../formatter-bayesean-show/index.js'); //(outfile, callback)
-const {runBookPredictor} = require('../predictor-bayesean-book/runPredictor.js'); //(infile, outfile, callback)
-const {runShowPredictor} = require('../predictor-bayesean-show/runPredictor.js'); //(infile, outfile, callback)
-const {reformatBookOutput} = require('../predictor-bayesean-book/processOutput.js'); //(infile, outfile, callback)
-const {reformatShowOutput} = require('../predictor-bayesean-show/processOutput.js'); //(infile, outfile, callback)
-
