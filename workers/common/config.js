2055a534f0a08584ad1308d762e5aa206080e2f4
nalenz <julian.nalenz@tum.de>
2019-04-03 22:09:03 +0200

    Add API base URL to JS config file

--- workers/common/config.js
+++ workers/common/config.js
@@ -2,5 +2,6 @@ module.exports = {
   GOT_CURRENT_YEAR_BOOK: 300,
   GOT_CURRENT_YEAR_SHOW: 304,
   AGE_MAXIMUM: 100,
-  GOT_SHOW_BEGIN: 298 //first year the show took place
+  GOT_SHOW_BEGIN: 298, //first year the show took place
+  GOT_API_BASE_URL: 'https://gotdata.northeurope.cloudapp.azure.com/api',
 };
