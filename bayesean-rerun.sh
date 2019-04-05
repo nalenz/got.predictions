a6fa4def5ab727b2edfbf9365ad2a22413111d23
Valentin Dimov <balentin.dimov@gmail.com>
2019-04-05 11:00:48 +0200

    Added a shell script to completely rerun Bayesean predictions

---
+++ bayesean-rerun.sh
@@ -0,0 +1,7 @@
+node workers/formatter-bayesean-book
+node workers/formatter-bayesean-show
+node workers/predictor-bayesean-book
+node workers/predictor-bayesean-show
+node workers/postprocessor-bayesean-book
+node workers/postprocessor-bayesean-show
+node workers/uploader-predictions-bayesean
