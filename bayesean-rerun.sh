2b712a14820a661c797686d3c39ccbf685ff0535
nalenz <julian.nalenz@tum.de>
2019-04-10 00:44:51 +0200

    Update all Bayesian predictions

--- bayesean-rerun.sh
+++ bayesean-rerun.sh
@@ -1,11 +1,13 @@
-cd ./data/book
-./refetch.sh
-cd ../show
-./refetch.sh
+#!/bin/bash
+#cd ./data/book
+#./refetch.sh
+#cd ../show
+#./refetch.sh
+#cd ../..
 node workers/formatter-bayesean-book
 node workers/formatter-bayesean-show
 node workers/predictors-bayesian/predictor-bayesean-book
 node workers/predictors-bayesian/predictor-bayesean-show
 node workers/postprocessor-bayesean-book
 node workers/postprocessor-bayesean-show
-node workers/uploader-attributes-bayesean
+#node workers/uploader-attributes-bayesean
